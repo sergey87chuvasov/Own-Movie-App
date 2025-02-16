31eda00f9b35ab294a21f62edb5257ed253bf9ae
Sergey87 <serge.techno@gmail.com>
2025-02-16 23:57:02 +0300

    added dark mode functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import ThemeCom from '@/components/ThemeCom';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +20,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeCom>
+          <Header />
+        </ThemeCom>
         {children}
       </body>
     </html>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
+
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
@@ -13,12 +15,15 @@ export default function Header() {
           <Link href={'/about'}>About</Link>
         </li>
       </ul>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch />
         <Link href={'/'} className='flex gap-1 items-center'>
           <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
             IMDb
           </span>
           <span className='text-xl hidden sm:inline'>Clone</span>
         </Link>
+      </div>
     </div>
   );
 }
