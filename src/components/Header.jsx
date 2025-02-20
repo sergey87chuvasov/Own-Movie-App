41b52d03d5ce00e6ecc8fa791b5f6d436074fec1
Sergey87 <serge.techno@gmail.com>
2025-02-20 23:23:49 +0300

    added user authentication with Clerk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,34 @@
-import Link from 'next/link';
-import DarkModeSwitch from './DarkModeSwitch';
+import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
   return (
-    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
-      <ul className='flex gap-4'>
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <ul className="flex gap-4">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Link href={"/sign-in"}>Sign in</Link>
+        </SignedOut>
         <li>
-           <Link href={'/sign-in'}>Sign in</Link>
+          <Link href={"/sign-in"}>Sign in</Link>
         </li>
-        <li className='hidden sm:block'>
-          <Link href={'/'}>Home</Link>
+        <li className="hidden sm:block">
+          <Link href={"/"}>Home</Link>
         </li>
-        <li className='hidden sm:block'>
-          <Link href={'/about'}>About</Link>
+        <li className="hidden sm:block">
+          <Link href={"/about"}>About</Link>
         </li>
       </ul>
-      <div className='flex items-center gap-4'>
+      <div className="flex items-center gap-4">
         <DarkModeSwitch />
-        <Link href={'/'} className='flex gap-1 items-center'>
-          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
             IMDb
           </span>
-          <span className='text-xl hidden sm:inline'>Clone</span>
+          <span className="text-xl hidden sm:inline">Clone</span>
         </Link>
       </div>
     </div>
